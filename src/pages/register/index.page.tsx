73945395fd38a2c591f66d764480d263b32dd908
Jean Martins <jeanmrtns4@gmail.com>
2023-03-09 01:39:38 -0300

    feat: create user functionality

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -1,5 +1,7 @@
+import { api } from '@/lib/api'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
+import { AxiosError } from 'axios'
 import { useRouter } from 'next/router'
 import { ArrowRight } from 'phosphor-react'
 import { useEffect } from 'react'
@@ -43,7 +45,20 @@ export default function Register() {
   }, [router.query?.username, setValue])
 
   async function handleRegister(data: RegisterFormData) {
-    console.log(data)
+    try {
+      const response = await api.post('/users', {
+        name: data.name,
+        username: data.username,
+      })
+      console.log(response.data)
+    } catch (error) {
+      if (error instanceof AxiosError && error.response?.data.message) {
+        alert(error.response.data.message)
+        return
+      }
+
+      console.log(error)
+    }
   }
 
   return (
