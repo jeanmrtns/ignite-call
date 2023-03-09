aac7c1f92436394544194d40ac1a40ca9a82cfad
Jean Martins <jeanmrtns4@gmail.com>
2023-03-09 13:11:25 -0300

    feat: create connection page

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -46,11 +46,12 @@ export default function Register() {
 
   async function handleRegister(data: RegisterFormData) {
     try {
-      const response = await api.post('/users', {
+      await api.post('/users', {
         name: data.name,
         username: data.username,
       })
-      console.log(response.data)
+
+      await router.push('/register/connect-calendar')
     } catch (error) {
       if (error instanceof AxiosError && error.response?.data.message) {
         alert(error.response.data.message)
